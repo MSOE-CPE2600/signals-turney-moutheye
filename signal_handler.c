a49d7bee624ce5cd3876fc9d49b0245f0f60b1cf
moutheye <moutheye@msoe.edu>
2025-11-05 21:43:54 -0600

    signal_handler.c done

--- signal_handler.c
+++ signal_handler.c
@@ -1,12 +1,17 @@
 /**
  * @file signal_handler.c
  * @brief Sets a signal handler for SIGINT, the handler prints a message and then quits
+ * - ctrl + c interupts the program
+ * - can kill process id ex kill -SIGINT 12345 if process id is 12345
+ * 
  */
 
 /**
- * Modified by:
+ * Modified by: Elias Mouthey
  * 
  * Brief summary of modifications:
+ * if exit is removed the signal can be received but never exits
+ * shows that a signal is recieved continues printing sleeping
  */
 
 
@@ -18,18 +23,21 @@
 /**
  * @brief Signal handler for SIGINT - prints a message and exits
  */
-void handle_signal() {
+void handle_signal() 
+{
     printf("Received a signal\n");
-    exit(1);
+    //exit(1);
 }
 
-int main() {
+int main() 
+{
 
     // Register for the signal
     signal(SIGINT, handle_signal);
 
     // Wait until a signal is received
-    while(1) {
+    while(1) 
+    {
         printf("Sleeping\n");
         sleep(1);
     }
